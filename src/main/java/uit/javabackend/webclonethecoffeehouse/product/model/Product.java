f8638acd5d6fb6bd2c13def65062ad85192713ab
CoderKhanhDZ <khanhpro1234567890@gmail.com>
2022-10-28 13:54:45 +0700

    feat: format

--- src/main/java/uit/javabackend/webclonethecoffeehouse/product/model/Product.java
+++ src/main/java/uit/javabackend/webclonethecoffeehouse/product/model/Product.java
@@ -5,11 +5,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.validator.constraints.Length;
+import uit.javabackend.webclonethecoffeehouse.orderproduct.model.OrderProduct;
 import uit.javabackend.webclonethecoffeehouse.productgroup.model.ProductGroup;
 import uit.javabackend.webclonethecoffeehouse.common.model.BaseEntity;
 import uit.javabackend.webclonethecoffeehouse.currency.model.Currency;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Setter
 @Getter
@@ -18,29 +20,29 @@ import javax.persistence.*;
 @Entity
 @Table(name = ProductEntity.Product.TABLE_NAME)
 public class Product extends BaseEntity {
-    @Column(name = ProductEntity.Product.PRODUCT_NAME,unique = true)
+    @Column(name = ProductEntity.Product.NAME,unique = true)
     @Length(min = 5  , max = 100 ,message = "Product name must have length between {min} and {max}")
     private String name;
 
 
-    @Column(name = ProductEntity.Product.PRODUCT_PRICE)
+    @Column(name = ProductEntity.Product.PRICE)
     private Integer price;
 
     @ManyToOne // map id currency
-    @JoinColumn(name = "currency_id")
+    @JoinColumn(name = ProductEntity.Product.CURRENCY)
     private Currency currency;
 
     @ManyToOne
-    @JoinColumn(name = "productgroup_id")
+    @JoinColumn(name = ProductEntity.Product.PRODUCTGROUP)
     private ProductGroup productGroup;
 
-    @Column(name = ProductEntity.Product.PRODUCT_IMG_URL)
+    @OneToMany(mappedBy = ProductEntity.ProductMappedOrderProduct.PRODUCT_MAPPED_ODERPRODUCT)
+    private Set<OrderProduct> orderProducts;
+
+    @Column(name = ProductEntity.Product.IMG_URL)
     private String imgUrl;
 
-
-
-
-    @Column(name = ProductEntity.Product.PRODUCT_DESCRIPTION)
+    @Column(name = ProductEntity.Product.DESCRIPTION)
     @Length(min = 5  , max = 100 ,message = "Product description must have length between {min} and {max}")
     private String description;
 }
