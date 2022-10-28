1b2b000374b29f7009d157d8c63d5ffad1f1741d
CoderKhanhDZ <khanhpro1234567890@gmail.com>
2022-10-28 13:54:09 +0700

    feat: CJB-16, add-order-entity

---
+++ src/main/java/uit/javabackend/webclonethecoffeehouse/order/dto/OrderDTO.java
@@ -0,0 +1,34 @@
+package uit.javabackend.webclonethecoffeehouse.order.dto;
+
+import lombok.*;
+import org.hibernate.validator.constraints.Range;
+import uit.javabackend.webclonethecoffeehouse.product.validation.annotation.UniqueProductName;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDTO {
+
+        private UUID id;
+
+        @Size(min = 5 , max = 100,message = "order.name.size")
+        @NotBlank
+        private String name;
+
+        @NotBlank
+        private String address;
+
+        private String note;
+
+        private boolean useCoupon;
+
+        @Range(min = 1, message= "product.price.null")
+        private Integer totalPrice;
+
+}
