1db065783fefa72fd8006286911ff8b81dc0aa84
PhucHuynh2401 <phuc.huynh-van@banvien.com.vn>
2022-11-01 15:41:39 +0700

    feat: CJB-20, update user entity

--- src/main/java/uit/javabackend/webclonethecoffeehouse/user/repository/UserRepository.java
+++ src/main/java/uit/javabackend/webclonethecoffeehouse/user/repository/UserRepository.java
@@ -16,7 +16,7 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     @Query("select (count(u) > 0) from User u where u.email = ?1")
     boolean existsByEmail(String email);
 
-    void deleteByUsername(String name);
+    void deleteByUsername(String username);
 
-    Optional<User> findByUsername(String name);
+    Optional<User> findByUsername(String username);
 }
