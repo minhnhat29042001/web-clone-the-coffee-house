321589f2b7b3881bfaff5426993115c9e9c34160
PhucHuynh2401 <phuc.huynh-van@banvien.com.vn>
2022-11-01 10:47:42 +0700

    feat: CJB-20 Add user entity

---
+++ src/main/java/uit/javabackend/webclonethecoffeehouse/user/model/UserEntity.java
@@ -0,0 +1,18 @@
+package uit.javabackend.webclonethecoffeehouse.user.model;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UserEntity {
+    @UtilityClass
+    public class User {
+        public static final String TABLE_NAME = "TCH_USER";
+        public static final String USERNAME = "TCH_USERNAME";
+        public static final String PASSWORD = "TCH_PASSWORD";
+        public static final String EMAIL = "TCH_EMAIL";
+        public static final String PHONE = "TCH_PHONE";
+        public static final String GENDER = "TCH_GENDER";
+        public static final String AVATAR = "TCH_AVATAR";
+        public static final String BIRTH = "TCH_BIRTH";
+    }
+}
