5651e7b0fa23bb1278691c73901f30998f359e0f
PhucHuynh2401 <phuc.huynh-van@banvien.com.vn>
2022-12-13 14:45:28 +0700

    add cors configure

--- src/main/java/uit/javabackend/webclonethecoffeehouse/security/config/SecurityConfig.java
+++ src/main/java/uit/javabackend/webclonethecoffeehouse/security/config/SecurityConfig.java
@@ -14,10 +14,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import uit.javabackend.webclonethecoffeehouse.security.jwt.JwtAuthenticationFilter;
 import uit.javabackend.webclonethecoffeehouse.security.oauth.CustomOAuth2UserService;
 import uit.javabackend.webclonethecoffeehouse.security.oauth.RestAuthenticationEntryPoint;
 import uit.javabackend.webclonethecoffeehouse.security.oauth.handler.OAuth2AuthenticationSuccessHandler;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -56,9 +61,21 @@ public class SecurityConfig {
     }
 
     @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://web-clone-the-coffee-house-production.up.railway.app"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // CROSS ORIGIN
-        http.cors();
+        http.cors()
+                .and().csrf()
+                .disable();
         // DISABLE SESSION -> STATELESS
         http.sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
