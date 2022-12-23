7e566e7a452ce2fbdd66ee279397389f896ce6ac
PhucHuynh2401 <phuc.huynh-van@banvien.com.vn>
2022-12-23 14:44:06 +0700

    hot-fix/update security config to fix CORS issue

--- src/main/java/uit/javabackend/webclonethecoffeehouse/security/config/SecurityConfig.java
+++ src/main/java/uit/javabackend/webclonethecoffeehouse/security/config/SecurityConfig.java
@@ -1,11 +1,9 @@
 package uit.javabackend.webclonethecoffeehouse.security.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,15 +17,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import uit.javabackend.webclonethecoffeehouse.security.jwt.JwtAuthenticationFilter;
 import uit.javabackend.webclonethecoffeehouse.security.oauth.CustomOAuth2UserService;
 import uit.javabackend.webclonethecoffeehouse.security.oauth.RestAuthenticationEntryPoint;
 import uit.javabackend.webclonethecoffeehouse.security.oauth.handler.OAuth2AuthenticationSuccessHandler;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -35,8 +30,6 @@ public class SecurityConfig {
     private final UserDetailsService userDetailsService;
     private final JwtAuthenticationFilter authenticationFilter;
     private final CustomOAuth2UserService customOAuth2UserService;
-    @Autowired
-    private ListAllowedOriginConfig listAllowedOriginConfig;
     private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 
     public SecurityConfig(UserDetailsService userDetailsService, JwtAuthenticationFilter authenticationFilter, CustomOAuth2UserService customOAuth2UserService, OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler) {
@@ -69,11 +62,11 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://spectacular-clafoutis-af79ec.netlify.app","https://web-clone-the-coffee-house-production.up.railway.app","http://localhost"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://spectacular-clafoutis-af79ec.netlify.app", "https://web-clone-the-coffee-house-production.up.railway.app", "http://localhost"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
         configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type"));
-        configuration.setExposedHeaders(List.of("X-Get-Header"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
         configuration.setMaxAge(3600L);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
@@ -84,7 +77,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // CROSS ORIGIN
-        http.cors(Customizer.withDefaults());
+        http.cors();
         http.csrf().disable();
         // DISABLE SESSION -> STATELESS
         http.sessionManagement()
@@ -103,11 +96,11 @@ public class SecurityConfig {
                 .authenticationEntryPoint(new RestAuthenticationEntryPoint())
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/login/**", "/oauth2/**", "/oauth/**", "/auth/**", "/v3/api-docs/**", "/swagger-ui.html/**","/swagger-resources/**", "/swagger-ui/**","/webjars/**").permitAll()
-                .antMatchers( "/api/ProductsManagement/common/**", "/api/ProductGroupManagement/common/**", "/api/CurrencyMangement/common/**").permitAll()
-                .antMatchers( "/api/v1/business/common/**","/api/v1/discount/common/**").permitAll()
-                .antMatchers( "/api/v1/payment/billing-infomation").permitAll()
-                .antMatchers( "/api/Files/**").permitAll()
+                .antMatchers("/", "/login/**", "/oauth2/**", "/oauth/**", "/auth/**", "/v3/api-docs/**", "/swagger-ui.html/**", "/swagger-resources/**", "/swagger-ui/**", "/webjars/**").permitAll()
+                .antMatchers("/api/ProductsManagement/common/**", "/api/ProductGroupManagement/common/**", "/api/CurrencyMangement/common/**").permitAll()
+                .antMatchers("/api/v1/business/common/**", "/api/v1/discount/common/**").permitAll()
+                .antMatchers("/api/v1/payment/billing-infomation").permitAll()
+                .antMatchers("/api/Files/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
