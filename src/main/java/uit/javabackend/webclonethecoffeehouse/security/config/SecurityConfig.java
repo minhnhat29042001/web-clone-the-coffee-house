620b77f58bc2c46dd74df347a08e2ba2a36007ec
vanphuc0812 <vanphuc0812@gmail.com>
2022-12-25 21:42:44 +0700

    update SecurityConfig.java

--- src/main/java/uit/javabackend/webclonethecoffeehouse/security/config/SecurityConfig.java
+++ src/main/java/uit/javabackend/webclonethecoffeehouse/security/config/SecurityConfig.java
@@ -112,7 +112,7 @@ public class SecurityConfig {
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring().antMatchers("/js/**", "/images/**");
+        return web -> web.ignoring().antMatchers("/js/**", "/images/**", "/Files/**", "api/Files/**");
     }
 
 
